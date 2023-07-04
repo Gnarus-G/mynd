0255c400cdffce1b835a33adb7d2ab17221ae1dd
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2023-07-04 15:21:51 -0400

    feat: cool checkbox marking tasks as done

--- src/App.svelte
+++ src/App.svelte
@@ -69,15 +69,45 @@
   <ul class="overflow-y-auto container px-5" use:autoAnimate>
     {#each todos as todo, idx (todo.id)}
       <article
-        class="grid grid-cols-[max-content_auto_min-content] gap-5 my-2 items-start"
+        class="grid grid-cols-[max-content_auto_min-content] gap-5 my-2 items-center"
       >
-        <button
-          class="rounded bg-red-700 hover:bg-red-800 transition active:scale-90 px-2"
-          on:click={() => removeTodo(todo.id)}>&times;</button
-        >
+        <div id="done-toggle" class="inline-flex items-center">
+          <label
+            class="relative flex cursor-pointer items-center rounded-full p-3"
+            for="checkbox"
+          >
+            <input
+              type="radio"
+              class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-pink-500 checked:bg-pink-500 checked:before:bg-pink-500 hover:before:opacity-10"
+              id="checkbox"
+              checked={todo.done}
+              on:click={() => removeTodo(todo.id)}
+            />
+            <div
+              class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-3.5 w-3.5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                stroke="currentColor"
+                stroke-width="1"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </label>
+        </div>
+
         <p
-          class="font-semibold hover:text-white break-words {todo.done &&
-            'line-through'}"
+          class="font-semibold hover:text-white break-words {todo.done
+            ? 'line-through'
+            : ''}"
         >
           {todo.message}
         </p>
