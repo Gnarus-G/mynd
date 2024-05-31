d785f968f8a2eed9a5c9f4f77172ac88510c778e
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-05-30 22:41:55 -0400

    fix: install to user local bin

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ set -xe;
 pnpm tauri build;
 cd src-tauri;
 cargo build --release -p todo;
-sudo cp target/release/mynd target/release/todo /usr/local/bin/
+sudo cp target/release/mynd target/release/todo ~/.local/bin/
