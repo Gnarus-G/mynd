797d19361f3df93c2007214deffa3b7ac16469b3
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-06-12 10:54:03 -0400

    feat: better ui for input and buttons

---
+++ src/lib/TodoInput.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { addTodo, cleanTodos } from "./store";
+</script>
+
+<form
+  class="sticky bottom-4 container flex gap-5 px-8 items-center"
+  on:submit|preventDefault={(e) => {
+    const el = e.currentTarget.elements.namedItem("todo");
+    // @ts-ignore
+    addTodo(el.value);
+    // @ts-ignore
+    el.value = "";
+  }}
+>
+  <button
+    type="button"
+    class="rounded-full bg-rose-800 hover:bg-rose-900 transition active:scale-90 p-2 shadow-md shadow-slate-800 hover:shadow-sm"
+    on:click={cleanTodos}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      fill="#e8eaed"
+      ><path
+        d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z"
+      /></svg
+    >
+  </button>
+  <input class="rounded-md px-1 w-full h-full" name="todo" />
+  <button
+    class="flex gap-2 p-2 rounded-lg bg-slate-950 hover:bg-slate-900 transition active:scale-90 shadow-md shadow-slate-800 hover:shadow-sm"
+    type="submit"
+    ><strong>Add</strong>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      fill="#e8eaed"
+      ><path
+        d="M840-680v480q0 33-23.5 56.5T760-120H200q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h480l160 160Zm-80 34L646-760H200v560h560v-446ZM480-240q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35ZM240-560h360v-160H240v160Zm-40-86v446-560 114Z"
+      /></svg
+    ></button
+  >
+</form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,13 @@
   import {
     todos,
     load,
-    addTodo,
     removeTodo,
     moveUp,
     moveDown,
     moveBelow,
-    cleanTodos,
   } from "$lib/store";
   import TrashCan from "$lib/trash/TrashCan.svelte";
+  import TodoInput from "$lib/TodoInput.svelte";
 
   onMount(load);
 </script>
@@ -23,28 +22,6 @@
 <main
   class="h-screen overflow-y-auto bg-gray-800 text-gray-300 [&_input]:bg-gray-700 flex flex-col items-center justify-center gap-10"
 >
-  <form
-    class="sticky top-4"
-    on:submit|preventDefault={(e) => {
-      const el = e.currentTarget.elements.namedItem("todo");
-      // @ts-ignore
-      addTodo(el.value);
-      // @ts-ignore
-      el.value = "";
-    }}
-  >
-    <button
-      type="button"
-      class="rounded bg-pink-800 hover:bg-pink-900 transition active:scale-90 px-1 shadow-md shadow-slate-800 hover:shadow-sm"
-      on:click={cleanTodos}>&#x1F5D1;</button
-    >
-    <input class="rounded px-1" name="todo" />
-    <button
-      class="rounded-tr-lg rounded bg-slate-400 hover:bg-slate-500 transition active:scale-90 px-1 shadow-md shadow-slate-800 hover:shadow-sm"
-      type="submit">&#x1F4BE;</button
-    >
-  </form>
-
   <ul class="overflow-y-auto container px-5" use:autoAnimate>
     {#each $todos as todo, idx (todo.id)}
       <TodoItem
@@ -58,6 +35,8 @@
       />
     {/each}
   </ul>
+
+  <TodoInput />
 </main>
 
 <TrashCan />
